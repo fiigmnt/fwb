9914a61b2b34ea038a80986f28f2762036390d4a
fiigmnt <fiigmnt@protonmail.com>
2023-08-03 00:47:10 -0400

    service worker

--- next.config.js
+++ next.config.js
@@ -3,7 +3,8 @@ const withPWA = require('next-pwa')({
     disable: process.env.NODE_ENV === 'development',
     register: true,
     scope: '/app',
-    sw: 'service-worker.js',
+    // sw: 'service-worker.js',
+    sw: '/sw.js',
     //...
   })
   
