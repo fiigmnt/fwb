10df26d492713905432612c87828421e91f3d9e9
fiigmnt <fiigmnt@protonmail.com>
2023-08-03 00:59:13 -0400

    service worker

--- next.config.js
+++ next.config.js
@@ -1,13 +1,13 @@
-const withPWA = require('next-pwa')({
-    dest: 'public',
-    disable: process.env.NODE_ENV === 'development',
-    register: true,
-    scope: '/app',
-    // sw: 'service-worker.js',
-    sw: '/sw.js',
-    //...
-  })
-  
-  module.exports = withPWA({
-    // next.js config
-  })
+const withPWA = require("next-pwa")({
+  dest: "public",
+  disable: process.env.NODE_ENV === "development",
+  register: true,
+//   scope: "/app",
+  // sw: 'service-worker.js',
+  sw: "/sw.js",
+  //...
+});
+
+module.exports = withPWA({
+  // next.js config
+});
