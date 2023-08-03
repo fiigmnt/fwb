f443266748e81c32530cf37e890c5a992c92d8e8
fiigmnt <fiigmnt@protonmail.com>
2023-08-03 03:32:34 -0400

    trying PWA

--- app/page.tsx
+++ app/page.tsx
@@ -4,15 +4,28 @@
  * @author fiig <fiig@mirage.ar> | July 26, 2023 | Updated: August 2, 2023
  * ----------------------------------------------------------------------------------*/
 
+"use client";
 import React from "react";
+import Head from "next/head";
 import Header from "../components/Header";
 import Mapbox from "../components/Map";
 
 import styles from "./page.module.css";
 
+const names = ['checkered', 'crayon', 'cubism', 'checkered', 'light'];
+
 export default function Home() {
+  console.log('here')
   return (
     <>
+    <Head>
+    {names.map((name) => (
+          <>
+            <link key={name} rel="preload" href={`/models/${name}.glb`} as="model" />
+            <link key={name} rel="preload" href={`/models/${name}.usdz`} as="model" />
+          </>
+        ))}
+    </Head>
       <main className={styles.main}>
         <Header />
         <div className={styles.map}>
--- next.config.js
+++ next.config.js
@@ -1,8 +1,43 @@
 const withPWA = require("next-pwa")({
-  dest: 'public',
-  disable: process.env.NODE_ENV === 'development',
+  dest: "public", // destination
+  disable: process.env.NODE_ENV === "development", // disable in development mode
   register: true,
-  sw: '/sw.js',
+  sw: "/sw.js",
+  runtimeCaching: [
+    {
+      urlPattern: /\.(?:usdz|glb)$/, // the URL pattern for your models
+      handler: "CacheFirst",
+      options: {
+        cacheName: "3d-models-cache",
+        expiration: {
+          maxEntries: 20, // cache only the latest 20 entries
+          maxAgeSeconds: 30 * 24 * 60 * 60, // 30 days
+        },
+      },
+    },
+    {
+      urlPattern: /\/app\/.*\/page/, // match /app/[any-name]/page
+      handler: 'CacheFirst',
+      options: {
+        cacheName: 'dynamic-pages-cache',
+        expiration: {
+          maxEntries: 20,
+          maxAgeSeconds: 30 * 24 * 60 * 60, // 30 days
+        },
+      },
+    },
+    {
+      urlPattern: /\/app\/collect\/.*\/page/, // match /app/[any-name]/page
+      handler: 'CacheFirst',
+      options: {
+        cacheName: 'dynamic-pages-cache',
+        expiration: {
+          maxEntries: 20,
+          maxAgeSeconds: 30 * 24 * 60 * 60, // 30 days
+        },
+      },
+    }
+  ],
 });
 
 module.exports = withPWA({
